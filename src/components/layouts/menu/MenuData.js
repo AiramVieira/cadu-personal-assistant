d03b7eb53a905581602c91e4e1d350107ac6889a
AiramVieira <leonardo.airam12@gmail.com>
2021-09-28 11:44:49 -0300

    staged changes

---
+++ src/components/layouts/menu/MenuData.js
@@ -0,0 +1,16 @@
+import * as AiIcons from 'react-icons/ai';
+
+export const MenuData = [
+  {
+    title: 'Home',
+    path: '/inicio',
+    icon: <AiIcons.AiFillHome />,
+    cName: 'nav__text nav__list',
+  },
+  {
+    title: 'Search',
+    path: '/busca',
+    icon: <AiIcons.AiOutlineSearch />,
+    cName: 'nav__text nav__list',
+  },
+];
