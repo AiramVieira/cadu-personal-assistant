33d6bb8cc29c6a5d283f77d49dc3ff76fad3444e
AiramVieira <leonardo.airam12@gmail.com>
2021-10-20 18:56:47 -0300

    .

--- src/pages/discord-bot/CaduBot.js
+++ src/pages/discord-bot/CaduBot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const axios = require('axios');
+const { Player } = require('discord-music-player');
 
 const client = new Discord.Client({
   intents: [
@@ -13,56 +14,50 @@ const client = new Discord.Client({
   const url = `https://cadu-service-api.herokuapp.com`;
   const response = await axios.get(url);
 
-  console.log(response.data.key);
-
   const settings = {
     prefix: '!',
     token: response.data.key,
   };
 
-  const { Player } = require('discord-music-player');
   const player = new Player(client, {
     leaveOnEmpty: false,
   });
   client.player = player;
 
-  client.on('ready', () => {
-    console.log('I am ready to Play songs');
-  });
-
   client.login(settings.token);
-
-  client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-  });
-
-  client.on('messageCreate', async (message) => {
-    const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
-    const command = args.shift();
-    let guildQueue = client.player.getQueue(message.guild.id);
-
-    if (command === 'play') {
-      let queue = client.player.createQueue(message.guild.id);
-      await queue.join(message.member.voice.channel);
-      let song = await queue.play(args.join(' ')).catch((_) => {
-        if (!guildQueue) queue.stop();
-      });
-    }
-
-    if (command === 'skip') {
-      guildQueue.skip();
-    }
-
-    if (command === 'stop') {
-      guildQueue.stop();
-    }
-
-    if (command === 'pause') {
-      guildQueue.setPaused(true);
-    }
-
-    if (command === 'resume') {
-      guildQueue.setPaused(false);
-    }
-  });
 })();
+
+client.on('ready', () => {
+  console.log('I am ready to Play songs');
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', async (message) => {
+  const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
+  const command = args.shift();
+  let guildQueue = client.player.getQueue(message.guild.id);
+
+  if (command === 'play') {
+    let queue = client.player.createQueue(message.guild.id);
+    await queue.join(message.member.voice.channel);
+    let song = await queue.play(args.join(' ')).catch((_) => {
+      if (!guildQueue) queue.stop();
+    });
+  }
+
+  if (command === 'skip') {
+    guildQueue.skip();
+  }
+
+  if (command === 'stop') {
+    guildQueue.stop();
+  }
+
+  if (command === 'pause') {
+    guildQueue.setPaused(true);
+  }
+
+  if (command === 'resume') {
+    guildQueue.setPaused(false);
+  }
+});
